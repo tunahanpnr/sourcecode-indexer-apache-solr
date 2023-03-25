5af41830ccc9991e019b3760a920aba046ea1eec
tunahanpnr <tunahanpnr1@gmail.com>
2023-03-26 02:29:32 +0300

    feat: fetch elastic search-ui and clean the project
    elastic search-ui is fetched. all the unnecessary things removed. app returns hello world for now.

---
+++ search-ui/src/App.tsx
@@ -0,0 +1,14 @@
+// @ts-nocheck
+
+import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import "./custom.css";
+
+
+
+
+
+export default function App() {
+    return (
+        <div>Hello World</div>
+    );
+}

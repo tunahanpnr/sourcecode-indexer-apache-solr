a565077765cbd0f3f49faccedb938e160ca33108
tunahanpnr <tunahanpnr1@gmail.com>
2023-03-26 02:38:15 +0300

    feat: implement template from example and generate config

--- search-ui/src/App.tsx
+++ search-ui/src/App.tsx
@@ -2,13 +2,75 @@
 
 import "@elastic/react-search-ui-views/lib/styles/styles.css";
 import "./custom.css";
+import {
+    ErrorBoundary,
+    SearchProvider,
+    SearchBox,
+    Results,
+    PagingInfo,
+    ResultsPerPage,
+    Paging,
+    WithSearch
+} from "@elastic/react-search-ui";
+import {Layout} from "@elastic/react-search-ui-views";
 
 
 
-
+const config = {
+    searchQuery: {
+        search_fields: {
+            filename: {},
+            content_type: {},
+            content: {},
+        },
+        result_fields: {
+            filename: {},
+            content_type: {},
+            content: {},
+            created_date: {},
+            id: {},
+            _version_: {},
+        },
+        disjunctiveFacets: {},
+        facets: {},
+    },
+    autocompleteQuery: {},
+    alwaysSearchOnInitialLoad: false
+};
 
 export default function App() {
     return (
-        <div>Hello World</div>
+        <SearchProvider config={config}>
+            <WithSearch mapContextToProps={({wasSearched}) => ({wasSearched})}>
+                {({wasSearched}) => {
+                    return (
+                        <div className="App">
+                            <ErrorBoundary>
+                                <Layout
+                                    header={
+                                        <SearchBox autocompleteSuggestions={true}/>
+                                    }
+                                    sideContent={
+                                        <div></div>
+                                    }
+                                    bodyContent={
+                                        <Results
+                                            shouldTrackClickThrough={true}
+                                        />
+                                    }
+                                    bodyHeader={
+                                        <React.Fragment>
+                                            {wasSearched && <PagingInfo />}
+                                            {wasSearched && <ResultsPerPage />}
+                                        </React.Fragment>
+                                    }
+                                    bodyFooter={<Paging/>}
+                                />
+                            </ErrorBoundary>
+                        </div>
+                    );
+                }}
+            </WithSearch>
+        </SearchProvider>
     );
 }

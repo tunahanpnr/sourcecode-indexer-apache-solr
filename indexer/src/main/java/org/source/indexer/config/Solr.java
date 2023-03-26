f4710736e2ca561a92dec1b815dc7577c0a9f05a
tunahanpnr <tunahanpnr1@gmail.com>
2023-03-26 16:36:52 +0300

    feat(docker): create docker and docker-compose files for app

--- indexer/src/main/java/org/source/indexer/config/Solr.java
+++ indexer/src/main/java/org/source/indexer/config/Solr.java
@@ -4,8 +4,15 @@ import org.apache.solr.client.solrj.impl.HttpSolrClient;
 
 
 public class Solr {
-    private static final String SOLR_COLLECTION_URL = "http://localhost:8983/solr/intellij_core";
+    private static final String SOLR_COLLECTION_URL = getSolrCollectionUrl();
 
+    private static String getSolrCollectionUrl() {
+        String solrCollectionUrl = System.getenv("SOLR_COLLECTION_URL");
+        if (solrCollectionUrl == null) {
+            solrCollectionUrl = "http://localhost:8983/solr/intellij_core";
+        }
+        return solrCollectionUrl;
+    }
     public static HttpSolrClient getSolrCollectionClient() {
         return new HttpSolrClient.Builder(SOLR_COLLECTION_URL).build();
     }
